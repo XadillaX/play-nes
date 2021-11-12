7e0031d3945f5e4b229ef0f9b6b287cb91d6ed25
Khaidi Chu <i@233.dev>
2021-11-12 19:53:42 +0800

    Use `jsnes` (#1)

---
+++ game.js
@@ -0,0 +1,123 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const { Controller, NES } = require('jsnes');
+const {
+  Font,
+  Image,
+  IntRect,
+  RenderWindow,
+  Sound,
+  SoundBuffer,
+  Sprite,
+  Text,
+  Texture,
+  VideoMode,
+} = require('sfml.js');
+const nomnom = require('nomnom');
+const sleep = require('mz-modules/sleep');
+
+const opts = nomnom.options({
+  frameRate: {
+    abbr: 'f',
+    help: 'The frame rate. (defaults to 60)',
+    default: 60,
+  },
+  rom: {
+    position: 0,
+    help: 'The ROM file path.',
+    required: true,
+  }
+}).parse();
+
+const font = new Font();
+font.loadFromFileSync(path.join(__dirname, './fixedsys.ttf'));
+const fpsText = new Text('FPS: -', font, 12);
+fpsText.setPosition(20, 20);
+
+const KBDController = require('./controller');
+
+const SCREEN_WIDTH = 256;
+const SCREEN_HEIGHT = 240;
+
+const soundBuffer = new SoundBuffer();
+const sound = new Sound();
+const window = new RenderWindow(
+  new VideoMode(SCREEN_WIDTH * 2, SCREEN_HEIGHT * 2),
+  'NES Emulator',
+  RenderWindow.Style.Close | RenderWindow.Style.Titlebar);
+window.setFramerateLimit(opts.frameRate);
+
+const screenTexture = new Texture();
+const screenBuffer = Buffer.alloc(SCREEN_WIDTH * SCREEN_HEIGHT * 4);
+screenTexture.create(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+let currentSamples = [];
+const nes = new NES({
+  onFrame(frameBuffer) {
+    for (let i = 0; i < frameBuffer.length; i++) {
+      screenBuffer.writeUInt8(frameBuffer[i] >> 16, i * 4);
+      screenBuffer.writeUInt8((frameBuffer[i] >> 8) & 0xff, i * 4 + 1);
+      screenBuffer.writeUInt8((frameBuffer[i]) & 0xff, i * 4 + 2);
+      screenBuffer.writeUInt8(0xff, i * 4 + 3);
+    }
+    screenTexture.update(screenBuffer);
+  },
+  onStatusUpdate: console.log,
+  onAudioSample: function(left, right) {
+    currentSamples.push(left * 32768);
+  },
+});
+
+const p1 = new KBDController(nes, 1);
+const p2 = new KBDController(nes, 2);
+
+nes.loadROM(
+  fs.readFileSync(
+    path.resolve(process.cwd(), opts.rom),
+    { encoding: 'binary' }));
+
+(async () => {
+  let last = Date.now();
+  while (window.isOpen()) {
+    let event;
+    while ((event = window.pollEvent())) {
+      if (event.type === 'Closed' ||
+          (event.type === 'KeyPressed' && event.key.codeStr === 'Escape')) {
+        window.close();
+        process.exit(0);
+      } else if (event.type === 'KeyPressed') {
+        p1.keyDown(event.key.codeStr);
+        p2.keyDown(event.key.codeStr);
+      } else if (event.type === 'KeyReleased') {
+        p1.keyUp(event.key.codeStr);
+        p2.keyUp(event.key.codeStr);
+      }
+    }
+
+    nes.frame();
+    const now = Date.now();
+    const fps = (1000 / (now - last)).toFixed();
+    fpsText.setString(`FPS: ${fps}`);
+    last = now;
+
+    window.clear(0xffffffff);
+    const sprite = new Sprite(
+      screenTexture,
+      new IntRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT));
+    sprite.scale(2, 2);
+    window.draw(sprite);
+    window.draw(fpsText);
+    window.display();
+
+    soundBuffer.loadFromSamples(currentSamples, 1, 44100);
+    const sound = new Sound(soundBuffer);
+    sound.setVolume(100);
+    sound.play();
+    currentSamples = [];
+
+    await sleep(0);
+  }
+})();
--- lib/mappers/index.ts
+++
@@ -1,6 +0,0 @@
-import '../mapper';
-
-export * from './cnrom';
-export * from './nrom';
-export * from './sxrom';
-export * from './uxrom';
